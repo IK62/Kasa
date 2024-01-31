717d70513563234d635098d86a95cc47119c5ec6
IK62 <ismail.karrouch13@gmail.com>
2024-01-31 10:45:17 +0100

    added error page

--- src/components/errorPage.js
+++ src/components/errorPage.js
@@ -1,5 +1,15 @@
+import { useNavigate } from "react-router-dom"
+
 function ErrorPage() {
-  return <h1>404 Error</h1>
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <h1 className="errorTitle">404</h1>
+      <h2 className="errorText">Oups! La page que vous demandez n'existe pas.</h2>
+      <p className="errorLink" onClick={() => navigate("/")}>Retourner sur la page d’accueil</p>
+    </>
+  )
 }
 
 export default ErrorPage
